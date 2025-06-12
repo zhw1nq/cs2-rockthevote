ab9403c252a4b6fb9295f25642679b3e25ebde35
M-archand <13469995+M-archand@users.noreply.github.com>
2025-06-12 15:33:32 -0400

    Implement hud countdown + cleanup

--- CrossCutting/OnTickDisplay.cs
+++ CrossCutting/OnTickDisplay.cs
@@ -1,6 +1,4 @@
 using System.Text;
-using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Menu;
 using static CounterStrikeSharp.API.Core.Listeners;
 
 namespace cs2_rockthevote.CrossCutting
@@ -9,30 +7,28 @@ namespace cs2_rockthevote.CrossCutting
     {
         private readonly PluginState _pluginState;
         private readonly EndMapVoteManager _endMap;
+        private readonly RockTheVoteCommand _rtv;
+        private readonly ExtendRoundTimeManager _voteExtend;
         private readonly StringLocalizer _localizer;
-        private readonly GeneralConfig _generalConfig;
-        private readonly VoteTypeConfig _voteTypeConfig;
+        private GeneralConfig _generalConfig = new();
+        private VoteTypeConfig _voteTypeConfig = new();
         private EndOfMapConfig _endMapConfig = new();
         private VoteExtendConfig _voteExtendConfig = new();
         private RtvConfig _rtvConfig = new();
 
-        public OnTickDisplay(
-            PluginState pluginState,
-            StringLocalizer localizer,
-            GeneralConfig generalConfig,
-            VoteTypeConfig voteTypeConfig,
-            EndMapVoteManager endMap
-        )
+        public OnTickDisplay(PluginState pluginState, StringLocalizer localizer, EndMapVoteManager endMap, RockTheVoteCommand rtv, ExtendRoundTimeManager voteExtend)
         {
             _pluginState = pluginState;
             _localizer = localizer;
-            _generalConfig = generalConfig;
-            _voteTypeConfig = voteTypeConfig;
             _endMap = endMap;
+            _rtv = rtv;
+            _voteExtend = voteExtend;
         }
 
         public void OnConfigParsed(Config config)
         {
+            _generalConfig = config.General;
+            _voteTypeConfig = config.VoteType;
             _endMapConfig = config.EndOfMapVote;
             _voteExtendConfig = config.VoteExtend;
             _rtvConfig = config.Rtv;
@@ -48,23 +44,39 @@ namespace cs2_rockthevote.CrossCutting
 
         public void PlayerOnTick()
         {
-            // Only shown while the vote is running
-            if (!_pluginState.EofVoteHappening || !_pluginState.ExtendTimeVoteHappening || !PanoramaVote.IsVoteInProgress())
+            // Only shown while a vote is running
+            if (!_pluginState.EofVoteHappening && !_pluginState.ExtendTimeVoteHappening && !_pluginState.RtvVoteHappening)
                 return;
 
-            // EndMapVote HUD Countdown
-            if (_endMap.TimeLeft > 0 && _endMapConfig.EnableCountdown && _endMapConfig.HudCountdown)
+            // EndMapVote HUD Countdown. Don't show if EnabledHudMenu true, otherwise this would be covered by the map list
+            if (_endMapConfig.EnableCountdown && _endMapConfig.HudCountdown && _pluginState.EofVoteHappening && !_voteTypeConfig.EnableHudMenu)
             {
-                string countdown = _localizer.Localize("emv.hud.hud-timer", _endMap.TimeLeft);
+                string countdown = _localizer.Localize("emv.hud.timer", _endMap.TimeLeft);
                 foreach (var player in ServerManager.ValidPlayers())
                     player.PrintToCenter(countdown);
             }
 
-            // HUD vote list
-            if (_voteTypeConfig.EnableHudMenu)
+            // RTV HUD Countdown
+            if (_rtvConfig.EnableCountdown && _rtvConfig.HudCountdown && _pluginState.RtvVoteHappening)
+            {
+                string countdown = _localizer.Localize("general.hud-countdown", _rtv.TimeLeft);
+                foreach (var player in ServerManager.ValidPlayers())
+                    player.PrintToCenter(countdown);
+            }
+
+            // VoteExtend HUD Countdown
+            if (_voteExtendConfig.EnableCountdown && _voteExtendConfig.HudCountdown && _pluginState.ExtendTimeVoteHappening)
+            {
+                string countdown = _localizer.Localize("general.hud-countdown", _voteExtend.TimeLeft);
+                foreach (var player in ServerManager.ValidPlayers())
+                    player.PrintToCenter(countdown);
+            }
+
+            // HUD map vote list
+            if (_voteTypeConfig.EnableHudMenu && _pluginState.EofVoteHappening)
             {
                 var sb = new StringBuilder();
-                sb.Append($"<b><font color='yellow'>{_localizer.Localize("emv.hud.menu-title")}</font></b>");
+                sb.Append($"<b><font color='yellow'>{_localizer.Localize("emv.hud.timer", _endMap.TimeLeft)}</font></b>");
 
                 int idx = 1;
                 foreach (var kv in _endMap.CurrentVotes.OrderByDescending(x => x.Value).Take(_endMap.MaxOptionsHud))
