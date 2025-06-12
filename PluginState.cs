b85f98162b154bb1586a981b6a3a0bc01e1cc310
M-archand <13469995+M-archand@users.noreply.github.com>
2025-06-12 15:33:45 -0400

    Add RtvVoteHappening

--- PluginState.cs
+++ PluginState.cs
@@ -5,20 +5,17 @@
         public bool MapChangeScheduled { get; set; }
         public bool EofVoteHappening { get; set; }
         public bool ExtendTimeVoteHappening { get; set; }
+        public bool RtvVoteHappening { get; set; }
         public int MapExtensionCount { get; set; } = 0;
 
-        public PluginState()
-        {
-
-        }
-
-        public bool DisableCommands => MapChangeScheduled || EofVoteHappening || ExtendTimeVoteHappening;
+        public bool DisableCommands => MapChangeScheduled || EofVoteHappening || ExtendTimeVoteHappening || RtvVoteHappening;
 
         public void OnMapStart(string map)
         {
             MapChangeScheduled = false;
             EofVoteHappening = false;
             ExtendTimeVoteHappening = false;
+            RtvVoteHappening = false;
             MapExtensionCount = 0;
         }
     }
