723723163ae022c20808982fb75ec130d2756552
M-archand <13469995+M-archand@users.noreply.github.com>
2025-06-27 22:23:51 -0400

    Simplify !timeleft

--- Features/TimeLeftCommand.cs
+++ Features/TimeLeftCommand.cs
@@ -16,21 +16,12 @@ namespace cs2_rockthevote
         }
     }
 
-    public class TimeLeftCommand : IPluginDependency<Plugin, Config>
+    public class TimeLeftCommand(TimeLimitManager timeLimitManager, MaxRoundsManager maxRoundsManager, GameRules gameRules, IStringLocalizer stringLocalizer) : IPluginDependency<Plugin, Config>
     {
-        private TimeLimitManager _timeLimitManager;
-        private MaxRoundsManager _maxRoundsManager;
-        private readonly GameRules _gameRules;
-        private StringLocalizer _localizer;
-
-
-        public TimeLeftCommand(TimeLimitManager timeLimitManager, MaxRoundsManager maxRoundsManager, GameRules gameRules, IStringLocalizer stringLocalizer)
-        {
-            _gameRules = gameRules;
-            _localizer = new StringLocalizer(stringLocalizer, "timeleft.prefix");
-            _timeLimitManager = timeLimitManager;
-            _maxRoundsManager = maxRoundsManager;
-        }
+        private TimeLimitManager _timeLimitManager = timeLimitManager;
+        private MaxRoundsManager _maxRoundsManager = maxRoundsManager;
+        private readonly GameRules _gameRules = gameRules;
+        private StringLocalizer _localizer = new StringLocalizer(stringLocalizer, "rtv.prefix");
 
         public void CommandHandler(CCSPlayerController? player)
         {
@@ -49,38 +40,28 @@ namespace cs2_rockthevote
             {
                 if (_timeLimitManager.TimeRemaining > 1)
                 {
-                    TimeSpan remaining = TimeSpan.FromSeconds((double)_timeLimitManager.TimeRemaining);
-                    if (remaining.Hours > 0)
-                    {
-                        text = _localizer.LocalizeWithPrefix("timeleft.remaining-time-hour", remaining.Hours.ToString("00"), remaining.Minutes.ToString("00"), remaining.Seconds.ToString("00"));
-                    }
-                    else if (remaining.Minutes > 0)
-                    {
-                        text = _localizer.LocalizeWithPrefix("timeleft.remaining-time-minute", remaining.Minutes, remaining.Seconds);
-                    }
-                    else
-                    {
-                        text = _localizer.LocalizeWithPrefix("timeleft.remaining-time-second", remaining.Seconds);
-                    }
+                    TimeSpan remainingFull = TimeSpan.FromSeconds((double)_timeLimitManager.TimeRemaining);
+                    string remaining = remainingFull.ToString(@"hh\:mm\:ss");
+                    text = _localizer.Localize("general.timeleft", remaining);
                 }
                 else
                 {
-                    text = _localizer.LocalizeWithPrefix("timeleft.time-over");
+                    text = "Time over.";
                 }
             }
             else if (!_maxRoundsManager.UnlimitedRounds)
             {
                 if (_maxRoundsManager.RemainingRounds > 1)
-                    text = _localizer.LocalizeWithPrefix("timeleft.remaining-rounds", _maxRoundsManager.RemainingRounds);
+                    text = $"Remaining Rounds: {_maxRoundsManager.RemainingRounds}";
                 else
-                    text = _localizer.LocalizeWithPrefix("timeleft.last-round");
+                    text = "Last round.";
             }
             else
             {
-                text = _localizer.LocalizeWithPrefix("timeleft.no-time-limit");
+                text = "No time limit.";
             }
 
-           if (player != null)
+            if (player != null)
                 player.PrintToChat(text);
             else
                 Server.PrintToConsole(text);
