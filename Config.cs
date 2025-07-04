2ddafe2d16227db74d237315167baa4e03f6c563
M-archand <13469995+M-archand@users.noreply.github.com>
2025-07-04 15:33:51 -0400

    Redo config, mostly voting menu type (WIP)

--- Config.cs
+++ Config.cs
@@ -16,20 +16,32 @@ namespace cs2_rockthevote
         public bool ChangeMapImmediatly { get; set; }
     }
 
-
-    public interface IEndOfMapConfig
+    public class RtvConfig : ICommandConfig, IVoteConfig
     {
-        public int MapsToShow { get; set; }
-        public bool ChangeMapImmediatly { get; set; }
-        public int VoteDuration { get; set; }
-        public bool SoundEnabled { get; set; }
-        public string SoundPath { get; set; }
+        public bool Enabled { get; set; } = true;
+        public bool EnabledInWarmup { get; set; } = false;
+        public bool EnablePanorama { get; set; } = true;
+        public int MinPlayers { get; set; } = 0;
+        public int MinRounds { get; set; } = 0;
+        public bool ChangeMapImmediatly { get; set; } = true;
+        public bool SoundEnabled { get; set; } = false;
+        public string SoundPath { get; set; } = "sounds/vo/announcer/cs2_classic/felix_broken_fang_pick_1_map_tk01.vsnd_c";
+        public int MapsToShow { get; set; } = 6;
+        public int RtvVoteDuration { get; set; } = 60;
+        public int MapVoteDuration { get; set; } = 60;
+        public int CooldownDuration { get; set; } = 180;
+        public int MapStartDelay { get; set; } = 180;
+        public int VotePercentage { get; set; } = 51;
+        public bool EnableCountdown { get; set; } = true;
+        public string CountdownType { get; set; } = "chat";
+        public int ChatCountdownInterval { get; set; } = 15;
     }
 
-    public class EndOfMapConfig : IEndOfMapConfig
+    public class EndOfMapConfig
     {
         public bool Enabled { get; set; } = true;
         public int MapsToShow { get; set; } = 6;
+        public string MenuType { get; set; } = "ScreenMenu";
         public bool ChangeMapImmediatly { get; set; } = false;
         public int VoteDuration { get; set; } = 150;
         public bool SoundEnabled { get; set; } = false;
@@ -39,28 +51,8 @@ namespace cs2_rockthevote
         public float DelayToChangeInTheEnd { get; set; } = 0F;
         public bool IncludeExtendCurrentMap { get; set; } = true;
         public bool EnableCountdown { get; set; } = false;
-        public bool HudCountdown { get; set; } = false;
-
-    }
-
-    public class RtvConfig : ICommandConfig, IVoteConfig, IEndOfMapConfig
-    {
-        public bool Enabled { get; set; } = true;
-        public bool EnabledInWarmup { get; set; } = false;
-        public bool NominationEnabled { get; set; } = true;
-        public int MinPlayers { get; set; } = 0;
-        public int MinRounds { get; set; } = 0;
-        public bool ChangeMapImmediatly { get; set; } = true;
-        public bool SoundEnabled { get; set; } = false;
-        public string SoundPath { get; set; } = "sounds/vo/announcer/cs2_classic/felix_broken_fang_pick_1_map_tk01.vsnd_c";
-        public int MapsToShow { get; set; } = 6;
-        public int VoteDuration { get; set; } = 60;
-        public int RtvVoteDuration { get; set; } = 45;
-        public int CooldownDuration { get; set; } = 180;
-        public int MapStartDelay { get; set; } = 180;
-        public int VotePercentage { get; set; } = 51;
-        public bool EnableCountdown { get; set; } = true;
-        public bool HudCountdown { get; set; } = false;
+        public string CountdownType { get; set; } = "chat";
+        public int ChatCountdownInterval { get; set; } = 30;
     }
 
     public class VotemapConfig : ICommandConfig, IVoteConfig
@@ -77,12 +69,23 @@ namespace cs2_rockthevote
     public class VoteExtendConfig
     {
         public bool Enabled { get; set; } = false;
+        public bool EnablePanorama { get; set; } = true;
         public int VoteDuration { get; set; } = 60;
         public int VotePercentage { get; set; } = 50;
         public int CooldownDuration { get; set; } = 180;
         public bool EnableCountdown { get; set; } = true;
-        public bool HudCountdown { get; set; } = false;
+        public string CountdownType { get; set; } = "chat";
+        public int ChatCountdownInterval { get; set; } = 15;
         public string Permission { get; set; } = "@css/vip";
+    }
+
+    public class NominateConfig
+    {
+        public bool Enabled { get; set; } = true;
+        public bool EnabledInWarmup { get; set; } = true;
+        public string MenuType { get; set; } = "ScreenMenu";
+        public int NominateLimit { get; set; } = 1;
+        public string Permission { get; set; } = "";
     }
 
     public class ScreenMenuConfig
@@ -95,20 +98,11 @@ namespace cs2_rockthevote
         public string SelectKey { get; set; } = "E";
     }
 
-    public class VoteTypeConfig
-    {
-        public bool EnableScreenMenu { get; set; } = false;
-        public bool EnableChatMenu { get; set; } = true;
-        public bool EnableHudMenu { get; set; } = false;
-        public bool EnablePanorama { get; set; } = true;
-    }
-
     public class GeneralConfig
     {
         public int MaxMapExtensions { get; set; } = 2;
         public int RoundTimeExtension { get; set; } = 15;
         public int MapsInCoolDown { get; set; } = 3;
-        public int ChatCountdownInterval { get; set; } = 20;
         public bool HideHudAfterVote { get; set; } = true;
         public bool RandomStartMap { get; set; } = false;
         public string DiscordWebhook { get; set; } = "";
@@ -117,13 +111,13 @@ namespace cs2_rockthevote
     public class Config : BasePluginConfig, IBasePluginConfig
     {
         [JsonPropertyName("ConfigVersion")]
-        public override int Version { get; set; } = 14;
+        public override int Version { get; set; } = 15;
         public RtvConfig Rtv { get; set; } = new();
+        public EndOfMapConfig EndOfMapVote { get; set; } = new();
+        public NominateConfig Nominate { get; set; } = new();
         public VotemapConfig Votemap { get; set; } = new();
         public VoteExtendConfig VoteExtend { get; set; } = new();
-        public EndOfMapConfig EndOfMapVote { get; set; } = new();
         public ScreenMenuConfig ScreenMenu { get; set; } = new();
-        public VoteTypeConfig VoteType { get; set; } = new();
         public GeneralConfig General { get; set; } = new();
     }
 }
